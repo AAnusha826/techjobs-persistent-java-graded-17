271dbd1b13c8d246f687504b028a27db8caeb6b3
AAnusha26 <adabalaanusha26@gmail.com>
2024-11-18 20:24:20 -0500

    techjobs-persistent-java

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 --Part 1
+-- Part 1: Columns and their data types in the job table
+-- id INT(11) AUTO_INCREMENT PRIMARY KEY
+-- title VARCHAR(255) NOT NULL
+-- description TEXT
+-- location VARCHAR(255)
+-- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 --Part 2
+-- Part 2: List names of employers in St. Louis City
+SELECT name FROM employer WHERE location = "St. Louis City";
+
 
 --Part 3
-
+DROP TABLE job;
 --Part 4
+SELECT * FROM skill LEFT JOIN  job_skills on skill.id = job_skills.skills_id where job_skills.jobs_id is not null order by name asc;
--- src/main/java/org/launchcode/techjobs/persistent/models/data/JobRepository.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/JobRepository.java
@@ -1,9 +1,9 @@
 package org.launchcode.techjobs.persistent.models.data;
 
 import org.launchcode.techjobs.persistent.models.Job;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobRepository extends CrudRepository<Job, Integer> {
+public interface JobRepository extends JpaRepository<Job, Integer> {
 }
